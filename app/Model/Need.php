4aea63f1e23254212a40f253dd3fc1f75165e60a
Ryan Chan <ryan@rcchan.com>
2013-02-19 17:31:04 -0500

    Needs model

---
+++ app/Model/Need.php
@@ -0,0 +1,33 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Need Model
+ *
+ */
+class Need extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'id' => array(
+			'naturalnumber' => array(
+				'rule' => array('naturalnumber'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
