8ab1ed99efd49ba4418d7fad6afbfbfceb39ad67
Ryan Chan <ryan@rcchan.com>
2013-02-24 20:46:00 -0500

    Utilization ignore disposed/decommissioned

--- app/Controller/Component/StatsComponent.php
+++ app/Controller/Component/StatsComponent.php
@@ -35,7 +35,7 @@ class StatsComponent extends Component {
     $data = $item->query('SELECT status, COUNT(*) as count FROM items GROUP BY status');
     $stats = array('status' => array(), 'utilization' => array());
     foreach($data as $d) $stats['status'][] = array($d['items']['status'], intval($d[0]['count']));
-    $data = $item->query('SELECT utilization, COUNT(*) as count FROM items GROUP BY utilization');
+    $data = $item->query('SELECT utilization, COUNT(*) as count FROM items WHERE status not in ("Disposed", "Decommissioned") GROUP BY utilization');
     foreach($data as $d) $stats['utilization'][] = array($d['items']['utilization'], intval($d[0]['count']));
     $this->controller->set(array('stats' => $stats)); // Sets data from myQuery in view
   }
