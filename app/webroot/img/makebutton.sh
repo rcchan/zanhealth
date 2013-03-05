fd2f084104479a6d1f9d3fdb8eec7b333e02a0f7
Ryan Chan <ryan@rcchan.com>
2013-03-05 01:49:26 -0500

    Remove unneeded files for production

--- app/webroot/img/makebutton.sh
+++
@@ -1,5 +0,0 @@
-if [ $# -ne 2 ]; then
-  echo "Usage: `basename $0` buttontext file"
-else
-  wget -q -O $2 "http://dabuttonfactory.com/b.png?t=$1&f=Calibri-Bold&ts=16&tc=ffffff&tshs=1&tshc=222222&it=png&c=10&bgt=gradient&bgc=808080&ebgc=000000&be=on&hp=20&vp=11"
-fi
